3061cec5610731608e92ab7fdd3d8753a725f8f7
Irvan Indriyan <42873448+irvanindriyan@users.noreply.github.com>
2025-02-07 13:41:22 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Controller;
 use App\Http\Middleware\OnlyJsonRequest;
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\Api\QuestionsController;
 
 Route::get('/', [
 	Controller::class, 
@@ -12,14 +13,34 @@ Route::get('/', [
 Route::middleware([
 	OnlyJsonRequest::class
 ])->prefix('questions')->group(function () {
-	Route::get('/', [App\Http\Controllers\Api\QuestionsController::class, 'callbackAnswer'])->name('questions');
-	Route::get('/1', [App\Http\Controllers\Api\QuestionsController::class, 'callbackAnswerOne'])->name('questions.1');
-	Route::post('/2', [App\Http\Controllers\Api\QuestionsController::class, 'callbackAnswerTwo'])->name('questions.2');
-	Route::post('/3', [App\Http\Controllers\Api\QuestionsController::class, 'callbackAnswerThree'])->name('questions.3');
-	Route::post('/4', [App\Http\Controllers\Api\QuestionsController::class, 'callbackAnswerFour'])->name('questions.4');
-	Route::post('/5', [App\Http\Controllers\Api\QuestionsController::class, 'callbackAnswerFive'])->name('questions.5');
-	Route::post('/6', [App\Http\Controllers\Api\QuestionsController::class, 'callbackAnswerSix'])->name('questions.6');
-	Route::post('/7', [App\Http\Controllers\Api\QuestionsController::class, 'callbackAnswerSeven'])->name('questions.7');
-	Route::post('/8', [App\Http\Controllers\Api\QuestionsController::class, 'callbackAnswerEight'])->name('questions.8');
-	Route::get('/9', [App\Http\Controllers\Api\QuestionsController::class, 'callbackAnswerNine'])->name('questions.9');
+	Route::get('/', [
+		QuestionsController::class, 'callbackAnswer'
+	])->name('questions');
+	Route::get('/1', [
+		QuestionsController::class, 'callbackAnswerOne'
+	])->name('questions.1');
+	Route::post('/2', [
+		QuestionsController::class, 'callbackAnswerTwo'
+	])->name('questions.2');
+	Route::post('/3', [
+		QuestionsController::class, 'callbackAnswerThree'
+	])->name('questions.3');
+	Route::post('/4', [
+		QuestionsController::class, 'callbackAnswerFour'
+	])->name('questions.4');
+	Route::post('/5', [
+		QuestionsController::class, 'callbackAnswerFive'
+	])->name('questions.5');
+	Route::post('/6', [
+		QuestionsController::class, 'callbackAnswerSix'
+	])->name('questions.6');
+	Route::post('/7', [
+		QuestionsController::class, 'callbackAnswerSeven'
+	])->name('questions.7');
+	Route::post('/8', [
+		QuestionsController::class, 'callbackAnswerEight'
+	])->name('questions.8');
+	Route::get('/9', [
+		QuestionsController::class, 'callbackAnswerNine'
+	])->name('questions.9');
 });
